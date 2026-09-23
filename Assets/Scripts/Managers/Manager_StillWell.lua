7e81cfc087386617ee8723424becb85da5a55bf6
FNFDev <fnf@devmail.com>
2026-09-22 20:38:04 -0400

    Added StillWell trigger scene with voice line sequence

---
+++ Assets/Scripts/Managers/Manager_StillWell.lua
@@ -0,0 +1,42 @@
+---@class Manager_StillWell : Behaviour
+local Manager_StillWell =
+{
+   elapsed = 0,
+   cutstep = 0,
+   VoiceSource = nil,
+   StartCut = false,
+   SoundPlaceHolder = nil,
+
+
+}
+
+function Manager_StillWell:OnStart()
+    self.VoiceSource = Resources.GetSound("Sounds\\Door_Close.ogg")
+    self.SoundPlaceHolder = self.owner:GetAudioSource()
+    self.SoundPlaceHolder:SetSound(self.VoiceSource)
+end
+
+function Manager_StillWell:OnTriggerEnter(other)
+    Debug.Log("Trigger entered!")
+   if other:GetOwner():GetTag() == "Cube" then
+        
+        self.StartCut = true
+    end
+end  
+
+function Manager_StillWell:OnUpdate(deltaTime)
+    self.elapsed = self.elapsed + deltaTime
+
+    if self.StartCut == true then
+        if self.elapsed > 5 and self.cutstep < 1 then
+            self.SoundPlaceHolder:Play()
+            Debug.Log("Hello, I've been expecting you.")
+            self.cutstep = 1
+        elseif self.elapsed > 25 and self.cutstep < 2 then
+            Debug.Log("I know you're still recovering after what happened, but... I have a task for you.")
+            self.cutstep = 2
+        end
+    end
+end
+
+return Manager_StillWell
