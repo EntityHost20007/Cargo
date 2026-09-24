3a2620f6bfd89aca3dafc16c407af5144349b4c5
FNFDev <fnf@devmail.com>
2026-09-23 20:16:44 -0400

    Add StillWell.ovscene with trigger voice lines; add EntityIntro.ovscene

--- Assets/Scripts/Managers/Manager_StillWell.lua
+++ Assets/Scripts/Managers/Manager_StillWell.lua
@@ -6,7 +6,8 @@ local Manager_StillWell =
    VoiceSource = nil,
    StartCut = false,
    SoundPlaceHolder = nil,
-
+   AnimPlaceHolder = nil,
+   AnimSource = nil,
 
 }
 
@@ -14,6 +15,7 @@ function Manager_StillWell:OnStart()
     self.VoiceSource = Resources.GetSound("Sounds\\Door_Close.ogg")
     self.SoundPlaceHolder = Scenes.GetCurrentScene():FindActorByName("StillWellVoice"):GetAudioSource()
     self.SoundPlaceHolder:SetSound(self.VoiceSource)
+    self.AnimPlaceHolder = Scenes.GetCurrentScene():FindActorByName("Character"):GetSkinnedMeshRenderer()
     
 end
 
@@ -30,11 +32,12 @@ function Manager_StillWell:OnUpdate(deltaTime)
 
     if self.StartCut == true then
         if self.elapsed > 5 and self.cutstep < 1 then
-            self.SoundPlaceHolder:Play()
+            self.AnimPlaceHolder:Stop()
             Debug.Log("Hello, I've been expecting you.")
             self.cutstep = 1
         elseif self.elapsed > 25 and self.cutstep < 2 then
             Debug.Log("I know you're still recovering after what happened, but... I have a task for you.")
+            self.AnimPlaceHolder:Play()
             self.cutstep = 2
         end
     end
